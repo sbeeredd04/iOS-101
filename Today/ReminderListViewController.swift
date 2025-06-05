efb1ecdcf5b12c9f5aadfb398b8e2db7e06bb77f
sbeeredd04 <sbeeredd@asu.edu>
2025-06-05 11:08:15 -0700

    Implemented today project, applied new contorller views

---
+++ Today/ReminderListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderListViewController.swift
+//  Today
+//
+//  Created by Sri Ujjwal Reddy B on 6/5/25.
+//
+
+import Foundation
---
+++ Today/Today/Models/Reminder.swift
@@ -0,0 +1,8 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by Sri Ujjwal Reddy B on 6/1/25.
+//
+
+import Foundation
